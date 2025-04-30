06a977d1f3f428922e24d0ca6e1a8993691d0bb8
sahithposa <sahith.posa1997.com>
2025-04-30 16:06:31 +0530

    devops

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "hello world, I am learning devops"
+
+PERSON1=$1  #this is first value from command line
+PERSON2=$2  #this is second value from command line
+echo "$PERSON1: Hi $PERSON2, How are you"
+echo "$PERSON2: Hi $PERSON1, Iam Good"
+echo "$PERSON1: Good morning $PERSON2"
+echo "$PERSON2: Good morning $PERSON1"
