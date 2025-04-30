c7e62e920f3a7ce2917efd9ea44093f498440d88
sahithposa <sahith.posa1997.com>
2025-04-30 15:53:21 +0530

    devops

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "hello world, I am learning devops"
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Sachin
+PERSON2=Rohit
 echo "$PERSON1: Hi $PERSON2, How are you"
 echo "$PERSON2: Hi $PERSON1, Iam Good"
 echo "$PERSON1: Good morning $PERSON2"
