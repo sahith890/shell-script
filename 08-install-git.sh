139b1126a7f11ed5ca28a97c7c4b2256953c90ef
sahithposa <sahith.posa1997.com>
2025-05-02 14:09:25 +0530

    condition

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -5,5 +5,7 @@ USERID=$(id-u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR: Please run this script with root access"
+    echo "ERROR:: Please run this script with root access"
 fi
+
+yum install git -y
