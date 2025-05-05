45d9b3bf13d7c552835b34ec7e6f2e11a96841ad
sahithposa <sahith.posa1997.com>
2025-05-05 16:15:42 +0530

    devops

--- 13-delete-oldlog.sh
+++ 13-delete-oldlog.sh
@@ -10,3 +10,11 @@ LOGFILE=$LOGDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+
+while read line
+do
+    echo "deleting $line"  &>>$LOGFILE
+    rm -rf $line
+
+done <<< $FILES_TO_DELETE
